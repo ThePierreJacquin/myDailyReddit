cf1332f610595c70176eb5053f09abd9dac6e47c
ThePierreJacquin <jacquinp@protonmail.com>
2024-09-25 12:12:36 +0400

    Initialized Streamlit Repo

---
+++ streamlit_app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.header("My Daily Reddit")
+st.subheader("Hello World")
+st.write("Access only a limited version of your reddit feeds, updated only once a day, to prevent doom scrolling without missing news you care about.")
